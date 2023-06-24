1b9b386ff9a286c5fed857a9acfaa042b0c8f417
Rounit Verma <rounitanand@gmail.com>
2023-06-24 14:58:10 +0530

    ExpenseForm fixed

--- src/components/Home/ExpenseForm.jsx
+++ src/components/Home/ExpenseForm.jsx
@@ -121,7 +121,7 @@ export default function ExpenseForm() {
 						</label>
 						<input
 							type="number"
-							placeholder="0.0 DH"
+							placeholder="0.0"
 							name="cost"
 							value={Fields.cost}
 							className={`${INPUT} ${
